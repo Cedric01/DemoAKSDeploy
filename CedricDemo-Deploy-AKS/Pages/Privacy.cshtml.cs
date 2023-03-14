daa00c4320ee601a0ed73e6bba26dcad06a36332
Cedric Kehi <kehiyann@hotmail.com>
2023-03-14 23:20:00 +0000

    first commit

---
+++ CedricDemo-Deploy-AKS/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CedricDemo_Deploy_AKS.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
